4d26616c2a9861c2003ec60ea075e533fe25d4ee
SRS <sebastian.rosnes.sorensen@hotmail.com>
2023-11-01 14:48:39 +0100

    made homepage more nice with shadows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,37 +13,68 @@ export default function Home() {
         <div className="w-full h-screen flex flex-col items-center justify-between">
           <div className="-mt-20 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+              <div>
+                <div className='relative isolate'>
+                  <div
+                    aria-hidden='true'
+                    className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+                    <div
+                      style={{
+                        clipPath:
+                          'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                      }}
+                      className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 
+                      rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] 
+                      sm:w-[72.1875rem]'
+                    />
+                  </div>
+                  <div>
+                    <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
 
-                {/* Text Section */}
-                <div className="max-w-4xl font-bold text-5xl md:text-6xl lg:text-7xl">
-                  <h1 className="text-blue-600 text-4xl md:text-5xl lg:text-6xl">
-                    Pregnancy Risk
-                  </h1>
+                      {/* Text Section */}
+                      <div className="max-w-4xl font-bold text-5xl md:text-6xl lg:text-7xl">
+                        <h1 className="text-blue-600 text-4xl md:text-5xl lg:text-6xl">
+                          Pregnancy Risk
+                        </h1>
 
-                  <p className="mt-4 text-xl lg:text-3xl">
-                    Instant assessment for a safer maternity.
-                  </p>
-                </div>
+                        <p className="mt-4 text-xl lg:text-3xl">
+                          Instant assessment for a safer maternity.
+                        </p>
+                      </div>
 
-                {/* Image Section */}
-                <div>
-                  <div className="flex items-center justify-center">
-                    <div className="flex items-center justify-center mt-16 sm:mt-24">
-                      <div className="rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4">
-                        <Image
-                          src='/pregnant.svg'
-                          //src='/gptpregnant.png'
-                          alt='product preview'
-                          width={400}
-                          height={350}
-                          quality={100}
-                          priority={true}
-                          className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
-                        />
+                      {/* Image Section */}
+                      <div>
+                        <div className="flex items-center justify-center">
+                          <div className="flex items-center justify-center mt-16 sm:mt-24">
+                            <div className="rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4">
+                              <Image
+                                src='/pregnant.svg'
+                                //src='/gptpregnant.png'
+                                alt='product preview'
+                                width={400}
+                                height={350}
+                                quality={100}
+                                priority={true}
+                                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                              />
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
+                </div>
+
+                <div
+                  aria-hidden='true'
+                  className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+                  <div
+                    style={{
+                      clipPath:
+                        'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+                  />
                 </div>
               </div>
             </div>
@@ -52,7 +83,8 @@ export default function Home() {
 
 
 
-        {/* <div>
+
+        <div>
           <div className='relative isolate'>
             <div
               aria-hidden='true'
@@ -69,8 +101,19 @@ export default function Home() {
             </div>
             <div>
 
+              <p className=" mb-10 max-w-prose text-zinc-700 sm:text-lg">
+                Welcome to Pregnancy Risk, your go-to resource for personalized pregnancy guidance.
+                Simply answer a few questions about yourself and <span className="font-bold">instantly </span>
+                receive a comprehensive risk assessment
+                for various health conditions and potential complications.
+              </p>
 
-
+              <Link className={buttonVariants({
+                size: "lg",
+                className: "mt-5"
+              })} href="/survey">
+                Get started <ArrowRight className="ml-2 h-5 w-5" />
+              </Link>
             </div>
 
             <div
@@ -85,21 +128,8 @@ export default function Home() {
               />
             </div>
           </div>
-        </div> */}
+        </div>
 
-        <p className=" mb-10 max-w-prose text-zinc-700 sm:text-lg">
-          Welcome to Pregnancy Risk, your go-to resource for personalized pregnancy guidance.
-          Simply answer a few questions about yourself and <span className="font-bold">instantly </span>
-          receive a comprehensive risk assessment
-          for various health conditions and potential complications.
-        </p>
-
-        <Link className={buttonVariants({
-          size: "lg",
-          className: "mt-5"
-        })} href="/survey">
-          Get started <ArrowRight className="ml-2 h-5 w-5" />
-        </Link>
 
 
       </PageWrapper>
@@ -132,7 +162,7 @@ export default function Home() {
               </span>
               <span className='mt-2 text-zinc-700'>
                 No sign-up required! Jump straight in and start the <Link
-                  href='/pricing'
+                  href='/survey'
                   className='text-blue-700'>
                   quiz
                 </Link> to share details about your pregnancy and health.
@@ -175,3 +205,42 @@ export default function Home() {
     </>
   )
 }
+
+
+
+// <div>
+// <div className='relative isolate'>
+//   <div
+//     aria-hidden='true'
+//     className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+//     <div
+//       style={{
+//         clipPath:
+//           'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+//       }}
+//       className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 
+//       rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] 
+//       sm:w-[72.1875rem]'
+//     />
+//   </div>
+//   <div>
+
+//     <p>NothingWelcome to Pregnancy Risk, your go-to resource for personalized pregnancy guidance.
+//       Simply answer a few questions about yourself and <span className="font-bold">instantly </span>
+//       receive a comprehensive r</p>
+
+//   </div>
+
+//   <div
+//     aria-hidden='true'
+//     className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+//     <div
+//       style={{
+//         clipPath:
+//           'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+//       }}
+//       className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+//     />
+//   </div>
+// </div>
+// </div>
